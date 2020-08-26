e22523183d61a586a17aed78ad3ecbd1013ff41c
everton42 <evertonferreira4596@gmail.com>
2020-08-25 22:19:24 -0300

    like and unlike registration

--- src/main/java/org/magnum/mobilecloud/video/VideoController.java
+++ src/main/java/org/magnum/mobilecloud/video/VideoController.java
@@ -21,17 +21,13 @@ package org.magnum.mobilecloud.video;
 import org.magnum.mobilecloud.video.repository.Video;
 import org.magnum.mobilecloud.video.repository.VideoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
-import retrofit.http.Body;
-import retrofit.http.POST;
 
-import java.util.UUID;
-import java.util.concurrent.atomic.AtomicLong;
+import java.util.ArrayList;
+import java.util.Collection;
 
 @RestController
 public class VideoController {
-
 	@Autowired
 	private VideoRepository repository;
 
@@ -39,6 +35,9 @@ public class VideoController {
 	public static final String DURATION_PARAMETER = "duration";
 	public static final String TOKEN_PATH = "/oauth/token";
 	public static final String VIDEO_SVC_PATH = "/video";
+	public static final String PARAM_ID = "/{id}";
+	private static final String LIKE_PATH = PARAM_ID+"/like";
+	private static final String UNLIKE_PATH = PARAM_ID+"/unlike";
 	public static final String VIDEO_TITLE_SEARCH_PATH = VIDEO_SVC_PATH + "/search/findByName";
 	public static final String VIDEO_DURATION_SEARCH_PATH = VIDEO_SVC_PATH + "/search/findByDurationLessThan";
 
@@ -46,5 +45,38 @@ public class VideoController {
 	public @ResponseBody Video addVideo (@RequestBody Video video){
 		repository.save(video);
 		return video;
+	}
+
+	@RequestMapping(value=VIDEO_SVC_PATH, method = RequestMethod.GET)
+	public @ResponseBody Collection<Video> getVideoList(){
+		Collection<Video> videos = new ArrayList<>();
+		repository.findAll().forEach(videos::add);
+		return videos;
+	}
+
+	@RequestMapping(value=VIDEO_SVC_PATH + LIKE_PATH, method = RequestMethod.POST)
+	public void likeVideo(@PathVariable("id") long id){
+		registerVideoInteraction(id, true);
+	}
+
+	@RequestMapping(value=VIDEO_SVC_PATH + UNLIKE_PATH, method = RequestMethod.POST)
+	public void unlikeVideo(@PathVariable("id") long id){
+		registerVideoInteraction(id, false);
+	}
+
+	@RequestMapping(value=VIDEO_SVC_PATH + PARAM_ID, method = RequestMethod.GET)
+	public @ResponseBody Video getVideoById(@PathVariable("id") long id){
+		return repository.findOne(id);
+	}
+
+	private void registerVideoInteraction(long id, boolean isLike) {
+		Video video = repository.findOne(id);
+		long likes = video.getLikes();
+
+		if (isLike) likes++;
+		else likes--;
+
+		video.setLikes(likes);
+		repository.save(video);
 	}
 }
